4e7eb6be8c5964d56519f255bb083bb6845e5565
Bobby Langley <bobbylangley227@gmail.com>
2021-04-16 16:10:31 -0400

    set up login/logout and framed the site a little

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, createContext } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import firebase from "firebase";
+import "./App.css";
+import { Col, Collapse, Layout, Row } from "antd";
+import { Content, Footer, Header } from "antd/lib/layout/layout";
+import Login from "./scenes/Login";
+import SignUp from "./scenes/Signup";
+import Home from "./scenes/Home";
+const { firebaseConfig } = require("./config");
+
+firebase.initializeApp(firebaseConfig);
+const firebaseAuth = firebase.auth();
+export const UserContext = createContext(null);
 
 function App() {
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <UserContext.Provider value={{ user, setUser, firebaseAuth }}>
+        <Router>
+          <Layout>
+            <Header style={{ color: "black" }}>This will be the header</Header>
+            <Row>
+              <Col>
+                <Content>
+                  <Switch>
+                    <Route path="/login" component={Login} />
+                    <Route path="/signup" component={SignUp} />
+                    <Route path="/" component={Home} />
+                  </Switch>
+                </Content>
+              </Col>
+            </Row>
+            <Footer style={{ textAlign: "center" }}>
+              ©2021 Created by Bobby Langley
+            </Footer>
+          </Layout>
+        </Router>
+      </UserContext.Provider>
+    </>
   );
 }
 
